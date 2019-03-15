7c32e0b83350c1ef4b6e7ce667ada7ebb59d2ece
forseyear <forseyear@users.noreply.github.com>
2019-03-15 10:38:59 +0900

    first commit

---
+++ components/code-editor.vue
@@ -0,0 +1,26 @@
+<template lang='pug'>
+  monaco-editor.editor(v-model="code" theme="vs-dark")
+</template>
+
+<script>
+import MonacoEditor from 'vue-monaco';
+import Config from '~/config';
+
+export default {
+  components: {
+    MonacoEditor
+  },
+  data() {
+    return {
+      code: Config.code
+    }
+  }
+}
+</script>
+
+<style lang='stylus'>
+div.editor
+  width 300px
+  height 500px
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template lang='pug'>
+  div.container
+    code-editor
+    //-registers
+</template>
+
+<script>
+import CodeEditor from '../components/code-editor';
+
+export default {
+  components: {
+    CodeEditor
+  },
+  data() {
+
+  }
+}
+</script>
+
+<style lang='stylus'>
+div.container
+  width 100%
+  height 100%
+</style>
+
